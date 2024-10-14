b341253b94d7f5e09dcc58073022b4edd7bc7729
Da Silva Barracha, Tiago Rafael <trsilvab@minsait.com>
2024-10-14 14:02:08 +0100

    added session change scroll to bottom

--- src/app/core/components/chatbot/chatbot-history/chatbot-history.component.ts
+++ src/app/core/components/chatbot/chatbot-history/chatbot-history.component.ts
@@ -1,10 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy, ElementRef, ViewChild } from '@angular/core';
 import { ChatbotMessageService } from '../../../services/chatbot/chatbot-message.service';
 import { ChatbotMessageComponent } from "../messages/chatbot-message/chatbot-message.component";
-import { UserMessageComponent } from "../messages/user-message/user-message.component";
 import { ChatMessage } from '../../../common/chatbot-models';
 import { ChatbotEventService } from '../../../services/chatbot/chatbot-event.service';
 import { Subscription } from 'rxjs';
+import { UserMessageComponent } from '../messages/user-message/user-message.component';
 
 @Component({
   selector: 'app-chatbot-history',
@@ -13,13 +13,15 @@ import { Subscription } from 'rxjs';
   templateUrl: './chatbot-history.component.html',
   styleUrls: ['./chatbot-history.component.scss']
 })
-export class ChatbotHistoryComponent implements OnInit {
+export class ChatbotHistoryComponent implements OnInit, OnDestroy {
+  @ViewChild('chatHistoryContainer') readonly chatHistoryContainer!: ElementRef<HTMLDivElement>;
+
   messages: ChatMessage[] = [];
   private sessionChangeSubscription!: Subscription;
 
   constructor(
-    private chatbotEventService: ChatbotEventService,
-    private chatbotMessageService: ChatbotMessageService
+    readonly chatbotEventService: ChatbotEventService,
+    readonly chatbotMessageService: ChatbotMessageService
   ) {}
 
   ngOnInit(): void {
@@ -29,12 +31,28 @@ export class ChatbotHistoryComponent implements OnInit {
       .getSessionChangeObservable()
       .subscribe(() => {
         this.messages = this.chatbotMessageService.currentSession.messages;
+        this.scrollToBottom();
       });
+
+    this.scrollToBottom();
   }
 
   ngOnDestroy(): void {
     if (this.sessionChangeSubscription) {
       this.sessionChangeSubscription.unsubscribe();
+    }
+  }
+
+  scrollToBottom(): void {
+    try {
+      setTimeout(() => {
+        this.chatHistoryContainer.nativeElement.scrollTo({
+          top: this.chatHistoryContainer.nativeElement.scrollHeight,
+          behavior: 'smooth'
+        });
+      }, 0);
+    } catch (err) {
+      console.error('Smooth scroll to bottom failed:', err);
     }
   }
 }
