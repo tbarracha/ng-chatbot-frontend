be46351b6097b89f9a9c48f0cccd447cbcc03a6b
Da Silva Barracha, Tiago Rafael <trsilvab@minsait.com>
2024-10-16 23:40:04 +0100

    code fixes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,6 @@
 import { Routes } from '@angular/router';
 import { PageChatbotComponent } from './pages/page-chatbot/page-chatbot.component';
-import { PageCompanyComponent } from './pages/page-company/page-company.component';
+import { PageCompanyComponent } from './pages/page-company-chatbot/page-company.component';
 
 export const routes: Routes = [
     { path: '', component: PageChatbotComponent },
